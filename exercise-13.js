9f60c30f386938380ec86b041e2a466c098ddc23
Max Glenister <moglenstar@gmail.com>
2014-06-21 15:57:25 +0100

    refactored 13

--- exercise-13.js
+++ exercise-13.js
@@ -4,34 +4,24 @@ var url = require('url');
 var new_time = {};
 
 var server = http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.writeHead(200, {"Content-Type": "application/json"});
     if (request.method === 'GET') {
-
         var req_url = url.parse(request.url, true);
+        var time = new Date(req_url.query.iso);
 
         if (req_url.pathname == '/api/parsetime') {
-            if (req_url.query.iso) {
-                var time = new Date(req_url.query.iso);
-
-                new_time = {
-                    'hour': time.getHours(),
-                    'minute': time.getMinutes(),
-                    'second': time.getSeconds()
-                }
-            }
+            new_time = JSON.stringify({
+                'hour': time.getHours(),
+                'minute': time.getMinutes(),
+                'second': time.getSeconds()
+            });
         }
         if(req_url.pathname == '/api/unixtime') {
-            if(req_url.query.iso) {
-                var time = new Date(req_url.query.iso);
-
-                new_time = {
-                    'unixtime': time.getTime()
-                }
-            }
+            new_time = JSON.stringify({
+                'unixtime': time.getTime()
+            });
         }
     }
-    new_time = JSON.stringify(new_time);
-
     response.end(new_time, 'utf8');
 });
 
